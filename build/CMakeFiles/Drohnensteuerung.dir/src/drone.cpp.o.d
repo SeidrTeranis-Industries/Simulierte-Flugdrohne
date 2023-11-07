fc19bf62635e69b73ee611ddd70c5ddea42ff29b
janripken <janripken@protonmail.com>
2023-11-07 22:37:11 +0100

    Entfernen von datein

--- build/CMakeFiles/Drohnensteuerung.dir/src/drone.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Drohnensteuerung.dir/src/drone.cpp.o: \
- /home/jan/SeidrTeranis\ Industries/SeidrTeranis\ Defense\ Systems/Simulierte-Flugdrohne/src/drone.cpp \
- /usr/include/stdc-predef.h
